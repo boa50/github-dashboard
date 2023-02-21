cd89b9f878d83c87cdeedbeddb4e725ad72f281b
boa50 <bruno.albuquerque50@gmail.com>
2023-02-21 18:04:02 -0300

    Created a function to get all repositories from my user

--- data-engeneering.R
+++ data-engeneering.R
@@ -15,7 +15,7 @@ con <- dbConnect(
 ### Call the GitHub api based on the passed url
 # Return a dataframe
 call_api <- function(url) {
-  get_details <- GET(
+  response <- GET(
     {{ url }},
     add_headers("Accept", "application/vnd.github+json"),
     add_headers("Authorization", paste("Bearer", Sys.getenv("API_TOKEN"))),
@@ -25,17 +25,54 @@ call_api <- function(url) {
   if (status_code(get_details) == 200) {
     df <- as.data.frame(
       fromJSON(
-        content(get_details, "text", encoding = "UTF-8"), 
+        content(response, "text", encoding = "UTF-8"),
         flatten = TRUE
       )
     )
+
+    return(df)
+  }
+
+  return(data.frame())
+}
+
+### Get my GitHub repositories
+get_repositories <- function() {
+  df_repositories <- data.frame()
+  
+  for (page in c(1:50)) {
+    url <- paste("https://api.github.com/users/boa50/repos?page=", page, sep = "")
+    df <- call_api(url)
     
     if (nrow(df) > 0) {
-      return(df[c("commit.message", "commit.author.date")])
-    } 
-  } 
+      df_repositories <- rbind(df_repositories, df["name"])
+    }
+    
+    if (nrow(df) < 30) {
+      break
+    }
+  }
   
-  return(data.frame())
+  return(df_repositories)
+}
+
+### Get the my commits at a single repository
+get_commits <- function(repository, next_date = "1970-01-01T00:00:00Z") {
+  url <- paste(
+    "https://api.github.com/repos/boa50/",
+    repository,
+    "/commits?author=boa50&since=", 
+    next_date, 
+    sep = ""
+  )
+  
+  df <- call_api(url)
+  
+  if (nrow(df) > 0) {
+    return(df[c("commit.message", "commit.author.date")])
+  }
+  
+  return(df)
 }
 
 ### Get the next date to start loading commits from the repository
@@ -63,22 +100,12 @@ insert_values <- function(df, con) {
   }
 }
 
-# url <- "https://api.github.com/repos/boa50/r-cheat-sheet/commits?since=2023-02-19T13:10:23Z"
 
-url <- "https://api.github.com/repos/boa50/r-cheat-sheet/commits?author=boa50"
-
-df <- call_api(url)
-
-# insert_values(df, con)
-
+############################# Program Execution ################################
+df_repositories <- get_repositories()
 
 next_date <- get_next_date()
 
-url <- paste(
-  "https://api.github.com/repos/boa50/r-cheat-sheet/commits?author=boa50&since=", 
-  data_str, 
-  sep = ""
-)
+df_commits <- get_commits(df_repositories[[2, 1]], next_date)
 
-df <- call_api(url)
-
+# insert_values(df, con)
