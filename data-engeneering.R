7f7691b902e003cbd1f20f5e5ec47e30d3b848eb
boa50 <bruno.albuquerque50@gmail.com>
2023-02-21 17:30:10 -0300

    Created a function to retrieve the next date to start loading the commits

--- data-engeneering.R
+++ data-engeneering.R
@@ -5,6 +5,13 @@ library(bigrquery)
 
 # usethis::edit_r_environ(scope = "project")
 
+con <- dbConnect(
+  bigquery(),
+  project = "github-dashboard-378513",
+  dataset = "interactions",
+  billing = "github-dashboard-378513"
+)
+
 ### Call the GitHub api based on the passed url
 # Return a dataframe
 call_api <- function(url) {
@@ -16,16 +23,43 @@ call_api <- function(url) {
   )
   
   if (status_code(get_details) == 200) {
-    return(
-      as.data.frame(
-        fromJSON(
-          content(get_details, "text", encoding = "UTF-8"), 
-          flatten = TRUE
-        )
-      )[c("commit.message", "commit.author.date")]
+    df <- as.data.frame(
+      fromJSON(
+        content(get_details, "text", encoding = "UTF-8"), 
+        flatten = TRUE
+      )
     )
+    
+    if (nrow(df) > 0) {
+      return(df[c("commit.message", "commit.author.date")])
+    } 
+  } 
+  
+  return(data.frame())
+}
+
+### Get the next date to start loading commits from the repository
+get_next_date <- function() {
+  tb_commits <- tbl(con, "commits")
+  
+  last_date <- tb_commits %>%
+    summarise(last_date = max(executed_date, na.rm = TRUE)) %>%
+    collect()
+  
+  next_date <- format(last_date[[1]] + 1, "%Y-%m-%dT%H:%M:%SZ")
+  
+  return(next_date)
+}
+
+### Insertign data into the BigQuery table
+insert_values <- function(df, con) {
+  if (nrow(df) > 0) {
+    names(df) <- c("message", "executed_date")
+    df$executed_date <- as.POSIXct(stringr::str_replace_all(df$executed_date, "Z|T", " "))
+    
+    dbWriteTable(con, "commits", df, append = TRUE)
   } else {
-    return(data.frame())
+    message("No rows to be inserted")
   }
 }
 
@@ -35,17 +69,16 @@ url <- "https://api.github.com/repos/boa50/r-cheat-sheet/commits?author=boa50"
 
 df <- call_api(url)
 
-### Insertign data into the BigQuery table
-if (nrow(df) > 0) {
-  names(df) <- c("message", "executed_date")
-  df$executed_date <- as.POSIXct(stringr::str_replace_all(df$executed_date, "Z|T", " "))
-  
-  con <- dbConnect(
-    bigquery(),
-    project = "github-dashboard-378513",
-    dataset = "interactions",
-    billing = "github-dashboard-378513"
-  )
-  
-  dbWriteTable(con, "commits", df, append = TRUE)
-}
+# insert_values(df, con)
+
+
+next_date <- get_next_date()
+
+url <- paste(
+  "https://api.github.com/repos/boa50/r-cheat-sheet/commits?author=boa50&since=", 
+  data_str, 
+  sep = ""
+)
+
+df <- call_api(url)
+
