aeb3b9e70af96afc64b8b6ea608468eee4327294
boa50 <bruno.albuquerque50@gmail.com>
2023-02-22 18:28:07 -0300

    Adapted the heatmap to the commits dataframe

--- tests.R
+++ tests.R
@@ -1,8 +1,6 @@
-library(tidyquant)
 library(plotly)
 library(ggplot2)
 library(dplyr)
-library(plyr)
 
 my_colours <- list(
   title = "#616161",
@@ -27,40 +25,38 @@ theme_boa <- function() {
     )
 }
 
-### References
+### Reference
 # http://www.columbia.edu/~sg3637/blog/Time_Series_Heatmaps.html
 
-### Loading the data
-amznStock <- as.data.frame(tidyquant::tq_get(c("AMZN"),get="stock.prices")) # get data using tidyquant
-amznStock <- amznStock[year(amznStock$date) > 2020, ]
+names(df_commits) <- c("message", "executed_date", "repository")
+df_commits$executed_date <- as.POSIXct(stringr::str_replace_all(df_commits$executed_date, "Z|T", " "))
 
-### Creating the heatmap
-amznStock$weekday = as.POSIXlt(amznStock$date)$wday #finding the day no. of the week
-amznStock$weekdayf<-factor(amznStock$weekday,levels=rev(1:7),labels=rev(c("Mon","Tue","Wed","Thu","Fri","Sat","Sun")),ordered=TRUE) # converting the day no. to factor
+df <- df_commits %>%
+  select(executed_date) %>% 
+  mutate(executed_date = as.Date(executed_date)) %>% 
+  count(executed_date) %>% 
+  mutate(weekday_number = wday(executed_date),
+         weekday = wday(executed_date, label = TRUE, locale = "en_AU.UTF-8"),
+         year = year(executed_date),
+         month = month(executed_date, label = TRUE, locale = "en_AU.UTF-8"),
+         week = week(executed_date)
+        )
 
-amznStock$monthf<-factor(month(amznStock$date),levels=as.character(1:12),labels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"),ordered=TRUE) # finding the month
-
-amznStock$yearmonth<- factor(as.yearmon(amznStock$date)) # finding the year and the month from the date. Eg: Nov 2018
-
-amznStock$week <- as.numeric(format(amznStock$date,"%W")) # finding the week of the year for each date
-
-amznStock<-ddply(amznStock,.(yearmonth),transform,monthweek=1+week-min(week)) # normalizing the week to start at 1 for every month
-
-p <- amznStock %>%
-# amznStock %>%
-  ggplot(aes(week, weekdayf, fill = adjusted, text = date)) + 
+# p <- df %>%
+df %>%
+  ggplot(aes(week, weekday, fill = n, text = executed_date)) + 
   geom_tile(height=0.7, width=0.7,) +
-  facet_wrap(~year(date), ncol = 1, scales = "fixed") +
+  facet_wrap(~year, ncol = 1, scales = "fixed") +
   # facet_wrap(~year(date), ncol = 1) + 
-  scale_fill_gradient(low="red", high="green") +  
-  scale_y_discrete(expand = c(0, 0), limits = rev(c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"))) +
-  scale_x_continuous(expand = c(0, 0)) +
+  scale_fill_gradient(low="#daeafa", high="#1976d2") +  
+  scale_y_discrete(expand = c(0, 0), limits = rev(c("Sun","Mon","Tue","Wed","Thu","Fri","Sat"))) +
+  scale_x_continuous(expand = c(0, 0), limits = c(1, 53)) +
   labs(
     title = "Time-Series Calendar Heatmap: AMZN Stock Prices",
     x = "Week",
     y = "",
-    fill = "Price"
+    fill = "Number of commits"
   ) +
   theme_boa()
 
-ggplotly(p)
+# ggplotly(p)
