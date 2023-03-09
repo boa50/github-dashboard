f7b68c301d0a7e76675967006cd692808dc56fd0
boa50 <bruno.albuquerque50@gmail.com>
2023-03-09 19:45:38 +1030

    Moved the calendar plot to the app

--- app.R
+++ app.R
@@ -1,50 +1,32 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(httr)
+library(jsonlite)
+library(dplyr)
+library(bigrquery)
+library(commits.calendar)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    titlePanel("For how long are you commiting"),
+    
+    fluidRow(
+      plotOutput("calendar", height = "600px")
     )
 )
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
+  con <- dbConnect(
+    bigquery(),
+    project = "github-dashboard-378513",
+    dataset = "interactions",
+    billing = "github-dashboard-378513"
+  )
+  
+  df_commits <- tbl(con, "commits") %>% collect()
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$calendar <- renderPlot({
+    plot_calendar(df_commits)
+  })
 }
 
 # Run the application 
